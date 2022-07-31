fbde27f833936b979c375944696deaf705a57224
sunhongfen <1164105342@qq.com>
2022-07-31 19:37:57 +0800

    新增test_git.java文件

---
+++ src/test/java/com/study/test_git.java
@@ -0,0 +1,17 @@
+package com.study;
+
+import org.testng.annotations.Test;
+
+/**
+ * @Project: lemon_test
+ * @Author: sun_h
+ * @Create: 2022-07-31 19:34
+ * @Desc：
+ **/
+
+public class test_git {
+    @Test
+    public void study_git(){
+        System.out.println("study_git");
+    }
+}
